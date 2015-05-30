9eeee3e4e2aad8f93ee5eca55e19df70ecbab2fc
Anton Paronen <anton.l.paronen@gmail.com>
2015-05-30 21:55:50 +0300

    Remove old linter shell script
    Sbt runs linters instead.

--- run-linters.sh
+++
@@ -1,8 +0,0 @@
-PROJECT_ROOT=$(readlink -f $(dirname $0))
-
-# scalastyle
-java -jar $PROJECT_ROOT/linters/scalastyle/scalastyle_2.11-0.6.0-batch.jar --config $PROJECT_ROOT/linters/scalastyle/scalastyle_config.xml $PROJECT_ROOT/app/controllers/
-
-# coffeelint
-coffeelint $PROJECT_ROOT/app/assets/javascripts/
-
