887be2b40b6221d14db702bf671ea36e49ec99b9
Anton Paronen <anton.l.paronen@gmail.com>
2015-06-07 16:53:15 +0300

    Do not deploy pull requests

--- scripts/travis-after-success.sh
+++ scripts/travis-after-success.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Deployment to Heroku
-if [ "$TRAVIS_BRANCH" = "master" ]; then
+if [ "$TRAVIS_BRANCH" = "master" ] || [ ! "$TRAVIS_PULL_REQUEST" ]; then
   sbt ++$TRAVIS_SCALA_VERSION stage deployHeroku
 else
-  echo "Branch is not master, skipping deployment."
+  echo "Branch is not master or this is a pull request, skipping deployment."
 fi
