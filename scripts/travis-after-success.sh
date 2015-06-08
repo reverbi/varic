0dff4ed19239efbd7bb588247c916c9b01516e87
Anton Paronen <anton.l.paronen@gmail.com>
2015-06-08 10:25:54 +0300

    Fix condition

--- scripts/travis-after-success.sh
+++ scripts/travis-after-success.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Deployment to Heroku
-if [ "$TRAVIS_BRANCH" = "master" ] && [ ! "$TRAVIS_PULL_REQUEST" ]; then
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = false ]; then
   sbt ++$TRAVIS_SCALA_VERSION stage deployHeroku
 else
   echo "Branch is not master or this is a pull request, skipping deployment."
