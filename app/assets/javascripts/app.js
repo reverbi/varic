83d8789e1ecb5b1ace5574661ed26151e2fcbd65
Anton Paronen <anton.l.paronen@gmail.com>
2015-06-15 08:37:12 +0300

    Remove unused constant

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,3 +1,1 @@
 var myApp = angular.module('myApp', ['ui.bootstrap', 'myApp.controllers']);
-
-myApp.constant('myConst', 12);
--- test/js/specs/testi.spec.js
+++ test/js/specs/testi.spec.js
@@ -1,25 +1,3 @@
-describe('Constant: myConst', function () {
-  var myConst;
-
-  beforeEach(function () {
-    module('myApp');
-
-    inject(function (_myConst_) {
-      myConst = _myConst_;
-    });
-  });
-
-  it('should exist', function () {
-    expect(!!myConst).toBe(true);
-  });
-
-  describe('the constant', function () {
-    it('should return my expected value', function () {
-      expect(myConst).toBe(12);
-    });
-  });
-});
-
 describe('ButtonController', function () {
   var ButtonController, scope;
 
